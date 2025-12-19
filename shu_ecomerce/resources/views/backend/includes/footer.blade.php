6feb8b5709405834bda0e8f83ac17afe609719e2
shuvo halder <shuvohalder4545@gmail.com>
2025-12-20 00:01:22 +0600

    footer is updated

--- shu_ecomerce/resources/views/backend/includes/footer.blade.php
+++ shu_ecomerce/resources/views/backend/includes/footer.blade.php
@@ -2,7 +2,7 @@
  <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2021</span>
+                        <span>this Admin panel made by shuvo Halder</span>
                     </div>
                 </div>
             </footer>
